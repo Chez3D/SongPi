a18b63c40fd8c4f584ed3018051845a3a56d2f4b
adam <achester.dev@gmail.com>
2026-08-30 20:53:19 +0100

    fix google tf model url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-wget "https://googleapis.com" -O yamnet.tflite
+wget "https://storage.googleapis.com/mediapipe-models/audio_classifier/yamnet/float32/1/yamnet.tflite" -O yamnet.tflite
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
