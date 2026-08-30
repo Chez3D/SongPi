e63b444df76b3d46f1332b0ff66efcfab1161950
adam <achester.dev@gmail.com>
2026-08-30 20:51:41 +0100

    fix google tf model url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-wget wget "https://googleapis.com" -O yamnet.tflite
+wget "https://googleapis.com" -O yamnet.tflite
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
